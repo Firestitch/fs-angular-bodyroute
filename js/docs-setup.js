87ac1378fb87b0c93ebd5f9ece1669ad1e91ed84
Raymond <ray@firestitch.com>
2015-12-21 08:46:00 -0500

    added

---
+++ js/docs-setup.js
@@ -0,0 +1,24 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "fs-angular-bodyroute.services:fsBodyroute",
+      "shortName": "fsBodyroute",
+      "type": "interface",
+      "moduleName": "fs-angular-bodyroute",
+      "keywords": "api fs-angular-bodyroute interface services"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
