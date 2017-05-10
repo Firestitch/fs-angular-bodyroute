001416f93abb6b4fe083182abdb83f0a295c5f6d
raygig <ray@firestitch.com>
2017-05-10 09:24:57 -0400

    Update service.js

--- src/app/scripts/services/service.js
+++ src/app/scripts/services/service.js
@@ -1,65 +1,114 @@
 (function () {
-    'use strict';
 
-    /**
-     * @ngdoc service
-     * @name fs.fsBodyroute
-    */
-    angular.module('fs-angular-bodyroute',[])
-    .factory('fsBodyroute', function ($location,$rootScope) {
+    'use strict';
 
-        return { init: init };
 
-        /**
-         * @ngdoc method
-         * @name init
-         * @methodOf fs.fsBodyroute
-         * @param {object} options The options to configure the body route
-         * @param {object} options.target ie: $rootScope
-         * @param {string} options.event ie: $routeChangeSuccess
-         */
-        function init(options) {
+    /**
 
-            var options = options || {};
+     * @ngdoc service
 
-            if(!options.target)
-                options.target = $rootScope;
+     * @name fs.fsBodyroute
 
-            if(!options.event)
-                options.event = '$stateChangeSuccess';
+    */
 
-            options.target.$on(options.event, function(event, state) {
-                apply(state);
-            });
-        }
+    angular.module('fs-angular-bodyroute',[])
 
-        function apply(state) {
+    .factory('fsBodyroute', function ($location,$rootScope) {
 
-            if(state.data && state.data.bodyRoute && state.data.bodyRoute.name) {
-                var parts = state.data.bodyRoute.name.split('-');
-            } else {
-                var parts = $location.path().replace(/\/\d+(\/|$)/g,'').replace(/(^\/|\/$)/,'').split("/");
-            }
 
-            if(!parts.length)
-                parts = ['index'];
+        return { init: init };
 
-            var body = angular.element(document.querySelector('body'));
-            body.attr('id','body-' + parts.join('-'));
 
-            angular.forEach(body.attr('class').split(' '),function(cls) {
-                if(cls.match(/^body-/)) {
-                    body.removeClass(cls);
-                }
-            });
+        /**
 
-            var namespace = 'body-';
-            angular.forEach(parts,function(name) {
-                body.addClass(namespace + name);
-                namespace += name + '-';
-            });
-        }
+         * @ngdoc method
 
-    });
+         * @name init
 
-})();
+         * @methodOf fs.fsBodyroute
+
+         * @param {object} options The options to configure the body route
+
+         * @param {object} options.target ie: $rootScope
+
+         * @param {string} options.event ie: $routeChangeSuccess
+
+         */
+
+        function init(options) {
+
+
+            var options = options || {};
+
+
+            if(!options.target)
+
+                options.target = $rootScope;
+
+
+            if(!options.event)
+
+                options.event = '$stateChangeSuccess';
+
+
+            options.target.$on(options.event, function(event, state) {
+
+                apply(state);
+
+            });
+
+        }
+
+
+        function apply(state) {
+
+
+            if(state.data && state.data.bodyRoute && state.data.bodyRoute.name) {
+
+                var parts = state.data.bodyRoute.name.split('-');
+
+            } else {
+
+                var parts = $location.path().replace(/\/\d+(\/|$)/g,'/').replace(/(^\/|\/$)/,'').split("/");
+
+            }
+
+
+            if(!parts.length)
+
+                parts = ['index'];
+
+
+            var body = angular.element(document.querySelector('body'));
+
+            body.attr('id','body-' + parts.join('-'));
+
+
+            angular.forEach(body.attr('class').split(' '),function(cls) {
+
+                if(cls.match(/^body-/)) {
+
+                    body.removeClass(cls);
+
+                }
+
+            });
+
+
+            var namespace = 'body-';
+
+            angular.forEach(parts,function(name) {
+
+                body.addClass(namespace + name);
+
+                namespace += name + '-';
+
+            });
+
+        }
+
+
+    });
+
+
+})();
