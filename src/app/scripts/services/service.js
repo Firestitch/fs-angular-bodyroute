29f3e6b42af91f1118700a662ab3cfbcf7d14771
raygig <ray@firestitch.com>
2017-08-01 15:34:21 -0400

    Update service.js

--- src/app/scripts/services/service.js
+++ src/app/scripts/services/service.js
@@ -94,7 +94,10 @@
 
             });
 
-
+            if(state.data.bodyClass) {
+                body.addClass(state.data.bodyClass);
+            }
+          
             var namespace = 'body-';
 
             angular.forEach(parts,function(name) {
